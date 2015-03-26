2f52e718fab1c4d0470d1f572ad67e8618bdcec7
Ben Eggers <beggerss@cs.washington.edu>
2015-03-26 13:26:56 -0700

    initial graphics crap

--- Main.hs
+++ Main.hs
@@ -2,6 +2,17 @@ module Main where
 
 
 import qualified Data.List as L
+import Graphics.GD
+import Foreign.C.Types
+import Control.Applicative
+
+
+-- used for image generation
+sq_size :: Int
+sq_size = 80
+
+border :: Int
+border = 10  -- pixels
 
 
 main :: IO ()
@@ -18,9 +29,15 @@ main = do
     s <- getLine
     let s' = parseSquare s
 
-    validate n' m' s'
+    putStrLn "Output file name (blank for none)?"
+    fname <- getLine
 
-    print $ tour n' m' s'
+    validate n' m' s'
+    let t = tour n' m' s'
+
+    drawTour t fname
+
+    print t
 
 
 type Square = (Int, Int)
@@ -33,9 +50,9 @@ parseSquare s = (read $ head res, read $ last res)
 
 validate :: Int -> Int -> Square -> IO ()
 validate n m (x, y)
-    | x < 0 || n < x    = error "Invalid x-coordinate"
-    | y < 0 || m < y    = error "Invalid y-coordinate"
-    | otherwise         = return ()
+    | x <= 0 || n < x = error "Invalid x-coordinate"
+    | y <= 0 || m < y = error "Invalid y-coordinate"
+    | otherwise      = return ()
 
 
 -- We'll use Warnsdorff's Rule (https://en.wikipedia.org/wiki/Knight%27s_tour) with
@@ -54,9 +71,8 @@ tour' remaining t s = do
 
 nextSquare :: [Square] -> [Square] -> Square -> Maybe Square
 nextSquare rs t s
-    | null nexts        = Nothing
-    | otherwise         = Just . snd . minimum $
-                            [(length (surrounding next rs), next) | next <- nexts]
+    | null nexts = Nothing
+    | otherwise  = Just . snd . minimum $ [(length (surrounding next rs), next) | next <- nexts]
         where nexts = surrounding s rs
 
 
@@ -64,4 +80,30 @@ surrounding :: Square -> [Square] -> [Square]
 surrounding (x, y) rs = filter (`elem` rs) [ (x + 1, y + 2), (x + 2, y + 1)
                                            , (x - 1, y + 2), (x + 2, y - 1)
                                            , (x - 1, y - 2), (x - 2, y - 1)
-                                           , (x + 1, y - 2), (x - 2, y + 1)]
+                                           , (x + 1, y - 2), (x - 2, y + 1)
+                                           ]
+
+
+drawTour :: Maybe [Square] -> String -> IO ()
+drawTour Nothing _ = return ()
+drawTour _ "" = return ()
+drawTour (Just sqs) fname = do
+    image <- let (x, y) = maximum sqs in newImage (x * sq_size + 2 * border, y * sq_size + 2 * border)
+    fillImage (rgb 0 0 0) image 
+    (sequence . getZipList) $ drawFilledRectangle <$> ZipList (map upLeft sqs) <*> ZipList (map lowRight sqs)
+            <*> ZipList (map getColor sqs) <*> ZipList (repeat image)
+    savePngFile (fname ++ ".png") image
+
+
+upLeft :: Square -> Point
+upLeft s = (0,0)
+
+
+lowRight :: Square -> Point
+lowRight s = (0,0)
+
+
+getColor :: Square -> Color
+getColor (x, y)
+    | even (x + y)  = rgb 100 100 100
+    | otherwise     = rgb 200 200 200
